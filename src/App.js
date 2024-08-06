0a9dbc14cc3fcd29069cba8d9ff6b0e5931a6c2c
Lakshmi Vangara <vlakshmi.a111@gmail.com>
2024-08-06 12:54:54 +0530

    insta commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch, Redirect} from 'react-router-dom'
+import {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import NotFound from './components/NotFound'
+import LoginPage from './components/LoginPage'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import ProtectedRoute from './components/ProtectedRoute'
+import SearchContext from './SearchContext'
+import UserProfile from './components/UserProfile'
+
+class App extends Component {
+  state = {isDark: false, searchInput: '', searchPostView: false}
+
+  onChangeTheme = () => {
+    this.setState(prev => ({isDark: !prev.isDark}))
+  }
+
+  clickButton = () => {
+    this.setState(prev => ({searchPostView: !prev.searchPostView}))
+  }
+
+  onEnterSearch = () => {
+    this.setState(prev => ({searchPostView: !prev.searchPostView}))
+  }
+
+  changeSearch = result => {
+    this.setState({searchInput: result})
+  }
+
+  render() {
+    const {isDark, searchInput, searchPostView} = this.state
+    return (
+      <SearchContext.Provider
+        value={{
+          searchInput,
+          clickSearchButton: this.clickButton,
+          changeSearchInput: this.changeSearch,
+          enterSearchButton: this.onEnterSearch,
+          searchPostView,
+          isDark,
+          changeTheme: this.onChangeTheme,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginPage} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/my-profile" component={Profile} />
+          <ProtectedRoute exact path="/users/:userId" component={UserProfile} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </SearchContext.Provider>
+    )
+  }
 }
-
-export default App;
+export default App
