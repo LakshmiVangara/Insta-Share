284f6d139f4c51b8c8c21ed4d139a85be02ea635
Lakshmi Vangara <vlakshmi.a111@gmail.com>
2024-08-06 18:01:28 +0530

    Fix ESLint errors

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,18 @@
-import SearchContext from '../../SearchContext';
-import Header from '../Header';
-import SearchPosts from '../SearchPosts';
-import Posts from '../Posts';
-import Stories from '../Stories';
+import SearchContext from '../../SearchContext'
+import Header from '../Header'
+import SearchPosts from '../SearchPosts'
+import Posts from '../Posts'
+import Stories from '../Stories'
 
-import './index.css';
+import './index.css'
 
 const Home = () => (
   <SearchContext.Consumer>
     {value => {
-      const {isDark, searchInput, searchPostView} = value;
+      const {isDark, searchInput, searchPostView} = value
       const appContainer = isDark
         ? 'app-container app-container2'
-        : 'app-container';
+        : 'app-container'
 
       return (
         <>
@@ -28,8 +28,8 @@ const Home = () => (
             )}
           </div>
         </>
-      );
+      )
     }}
   </SearchContext.Consumer>
-);
-export default Home;
+)
+export default Home
