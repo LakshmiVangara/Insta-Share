bba61c151d42c0a4fcc3104fcc1b2ee3ce04cb0a
LakshmiVangara <118714626+LakshmiVangara@users.noreply.github.com>
2024-08-06 16:45:43 +0530

    Update index.js

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,106 +1,101 @@
-import {Component} from 'react'
-import Cookies from 'js-cookie'
-import {Redirect} from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate, Navigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import './index.css';
 
-import './index.css'
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isCredential, setIsCredential] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
 
-class LoginPage extends Component {
-  state = {username: '', password: '', isCredential: false, errorMsg: ''}
+  const navigate = useNavigate();
+  const token = Cookies.get('jwt_token');
 
-  onSubmitForm = async event => {
-    event.preventDefault()
+  const onSubmitForm = async event => {
+    event.preventDefault();
 
-    const {username, password} = this.state
-    const userDetails = {username, password}
-    const loginApi = ' https://apis.ccbp.in/login'
+    const userDetails = { username, password };
+    const loginApi = 'https://apis.ccbp.in/login';
 
     const options = {
       method: 'POST',
       body: JSON.stringify(userDetails),
-    }
-    const response = await fetch(loginApi, options)
-    const data = await response.json()
+    };
+
+    const response = await fetch(loginApi, options);
+    const data = await response.json();
+
     if (response.ok) {
-      this.loginSuccess(data.jwt_token)
+      loginSuccess(data.jwt_token);
     } else {
-      this.loginFailure(data.error_msg)
+      loginFailure(data.error_msg);
     }
+  };
+
+  const loginSuccess = jwtToken => {
+    Cookies.set('jwt_token', jwtToken, { expires: 30, path: '/' });
+    navigate('/');
+  };
+
+  const loginFailure = error => {
+    setIsCredential(true);
+    setErrorMsg(error);
+  };
+
+  if (token !== undefined) {
+    return <Navigate to="/" />;
   }
 
-  onUsername = event => {
-    this.setState({username: event.target.value})
-  }
-
-  onPassword = event => {
-    this.setState({password: event.target.value})
-  }
-
-  loginSuccess = jwtToken => {
-    const {history} = this.props
-    Cookies.set('jwt_token', jwtToken, {expires: 30, path: '/'})
-    history.replace('/')
-  }
-
-  loginFailure = error => {
-    this.setState({isCredential: true, errorMsg: error})
-  }
-
-  render() {
-    const {username, password, isCredential, errorMsg} = this.state
-    const token = Cookies.get('jwt_token')
-
-    if (token !== undefined) {
-      return <Redirect to="/" />
-    }
-    return (
-      <div className="login-container">
+  return (
+    <div className="login-container">
+      <img
+        className="logo"
+        alt="website login"
+        src="https://res.cloudinary.com/dxjowybhg/image/upload/v1663949409/logo_ip0o9f.png"
+      />
+      <div className="login-details-container">
         <img
-          className="logo"
-          alt="website login"
-          src="https://res.cloudinary.com/dxjowybhg/image/upload/v1663949409/logo_ip0o9f.png"
+          className="website-logo"
+          alt="website logo"
+          src="https://res.cloudinary.com/dxjowybhg/image/upload/v1663949395/website-logo_gsc5ig.png"
         />
-        <div className="login-details-container">
-          <img
-            className="website-logo"
-            alt="website logo"
-            src="https://res.cloudinary.com/dxjowybhg/image/upload/v1663949395/website-logo_gsc5ig.png"
-          />
-          <h1 className="website-heading">Insta Share</h1>
-          <form className="form-container" onSubmit={this.onSubmitForm}>
-            <div className="input-container">
-              <label className="label" htmlFor="username">
-                USERNAME
-              </label>
-              <input
-                className="input"
-                value={username}
-                id="username"
-                placeholder="Username"
-                type="text"
-                onChange={this.onUsername}
-              />
-            </div>
-            <div className="input-container">
-              <label className="label" htmlFor="password">
-                PASSWORD
-              </label>
-              <input
-                className="input"
-                value={password}
-                id="password"
-                placeholder="Password"
-                type="password"
-                onChange={this.onPassword}
-              />
-            </div>
-            {isCredential && <p className="error">{errorMsg}</p>}
-            <button className="login-button" type="submit">
-              Login
-            </button>
-          </form>
-        </div>
+        <h1 className="website-heading">Insta Share</h1>
+        <form className="form-container" onSubmit={onSubmitForm}>
+          <div className="input-container">
+            <label className="label" htmlFor="username">
+              USERNAME
+            </label>
+            <input
+              className="input"
+              value={username}
+              id="username"
+              placeholder="Username"
+              type="text"
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <label className="label" htmlFor="password">
+              PASSWORD
+            </label>
+            <input
+              className="input"
+              value={password}
+              id="password"
+              placeholder="Password"
+              type="password"
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          {isCredential && <p className="error">{errorMsg}</p>}
+          <button className="login-button" type="submit">
+            Login
+          </button>
+        </form>
       </div>
-    )
-  }
-}
-export default LoginPage
+    </div>
+  );
+};
+
+export default LoginPage;
